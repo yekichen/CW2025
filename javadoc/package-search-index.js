67a3a8850711a01a7d6725712b0f15620c585cd9
shenchen <1410913055@qq.com>
2025-12-08 12:02:29 +0800

    docs: organise generated Javadoc into separate /javadoc folder

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"com.comp2042.logic"},{"l":"com.comp2042.logic.bricks"},{"l":"com.comp2042.logic.events"},{"l":"com.comp2042.logic.level"},{"l":"com.comp2042.ui"},{"l":"所有程序包","u":"allpackages-index.html"}];updateSearchResults();
